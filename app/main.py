a18ad604b787acae8c8cefa4c97cf868f97a9920
sebastien-john <thesebastienjohn@gmail.com>
2024-09-08 20:08:57 -0400

    basic flask application

--- app/main.py
+++ app/main.py
@@ -1,9 +1,10 @@
-from flask import Flask, request, jsonify
+from flask import Flask, render_template, request, jsonify
 from .model import summarize_text
 
 app = Flask(__name__)
 
 @app.route('/summarize', methods=['POST'])
+
 def summarize():
     data = request.json
     text = data.get('text')
@@ -12,6 +13,11 @@ def summarize():
     
     summary = summarize_text(text)
     return jsonify({'summary': summary})
+
+@app.route('/')
+
+def index():
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
